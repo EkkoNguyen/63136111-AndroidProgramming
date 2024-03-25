33921a779abaeaf6e631cc98458d78b90d8fc4db
Vinh_Nguyen <128900315+EkkoNguyen@users.noreply.github.com>
2024-03-25 12:03:10 +0700

    Thêm chức năng tính giờ khi chơi

--- ntu_63136111/Cau2_QuizzyApp/app/src/main/java/till/edu/cau2_quizzyapp/ManHinhChoiActivity.java
+++ ntu_63136111/Cau2_QuizzyApp/app/src/main/java/till/edu/cau2_quizzyapp/ManHinhChoiActivity.java
@@ -2,13 +2,32 @@ package till.edu.cau2_quizzyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.ProgressBar;
 
 public class ManHinhChoiActivity extends AppCompatActivity {
-
+    CountDownTimer countDownTimer;
+    int time = 20;
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_man_hinh_choi);
+
+        progressBar = findViewById(R.id.quiz_timer);
+        countDownTimer = new CountDownTimer(20000, 1000) {
+            @Override
+            public void onTick(long l) {
+                time = time - 1;
+                progressBar.setProgress(time);
+            }
+
+            @Override
+            public void onFinish() {
+                Dialog dialog = new Dialog(ManHinhChoiActivity.this);
+            }
+        }.start();
     }
 }
