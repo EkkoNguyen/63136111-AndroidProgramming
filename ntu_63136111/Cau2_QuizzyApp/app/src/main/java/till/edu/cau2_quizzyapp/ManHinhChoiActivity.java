a0de5d46681c2e05301b25503a8b41e358b89adb
Vinh_Nguyen <128900315+EkkoNguyen@users.noreply.github.com>
2024-03-25 09:44:04 +0700

    Tạo giao diện khi mới khởi động App

---
+++ ntu_63136111/Cau2_QuizzyApp/app/src/main/java/till/edu/cau2_quizzyapp/ManHinhChoiActivity.java
@@ -0,0 +1,14 @@
+package till.edu.cau2_quizzyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManHinhChoiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_man_hinh_choi);
+    }
+}
