eb0c9402288955514c0faed0802826132b5649e8
Vinh_Nguyen <128900315+EkkoNguyen@users.noreply.github.com>
2024-04-08 18:42:18 +0700

    RecyclerView Android
    Hiển thị danh sách các đối tượng

---
+++ UsingRecyclerView/app/src/main/java/till/edu/usingrecyclerview/LandScapeAdapter.java
@@ -0,0 +1,64 @@
+package till.edu.usingrecyclerview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class LandScapeAdapter extends RecyclerView.Adapter<LandScapeAdapter.ItemLandHolder> {
+    Context context;
+    ArrayList<LandScape> listData;
+
+    public LandScapeAdapter(Context context, ArrayList<LandScape> listData) {
+        this.context = context;
+        this.listData = listData;
+    }
+
+    @NonNull
+    @Override
+    public ItemLandHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater cai_bom = LayoutInflater.from(context);
+        View viewItem = cai_bom.inflate(R.layout.item_land, parent, false);
+
+        ItemLandHolder holderCreated = new ItemLandHolder(viewItem);
+        return holderCreated;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemLandHolder holder, int position) {
+        // Lấy đối tượng hiển thị
+        LandScape landScapeHienThi = listData.get(position);
+        // Lấy thông tin
+        String caption = landScapeHienThi.getLandCaption();
+        String tenFileAnh = landScapeHienThi.getLandImageFileName();
+        // Đặt vào các trường thông tin của holder
+        holder.tvCaption.setText(caption);
+        // Đặt ảnh
+            String packageName = holder.itemView.getContext().getPackageName();
+            int imageId = holder.itemView.getResources().getIdentifier(tenFileAnh, "mipmap", packageName);
+        holder.ivLandScape.setImageResource(imageId);
+    }
+
+    @Override
+    public int getItemCount() {
+        return listData.size();
+    }
+
+    static class ItemLandHolder extends RecyclerView.ViewHolder{
+        TextView tvCaption;
+        ImageView ivLandScape;
+
+        public ItemLandHolder(@NonNull View itemView) {
+            super(itemView);
+            tvCaption = itemView.findViewById(R.id.textViewCaption);
+            ivLandScape = itemView.findViewById(R.id.imageViewLand);
+        }
+    }
+}
